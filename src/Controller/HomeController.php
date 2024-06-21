8fe7ac86074e55b625f411e0bdaff8fa4c013c4b
juliengrima <gogrinformatique@gmail.com>
2024-06-21 16:38:36 +0200

    render newsletters controller new to layout page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\NewsLetters;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
--- src/Form/NewsLettersType.php
+++ src/Form/NewsLettersType.php
@@ -6,14 +6,28 @@ use App\Entity\NewsLetters;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class NewsLettersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-        ;
+            ->add('email', EmailType::class, [
+                'label' => 'Abonnez-vous à la newsletter et restez à l\'écoute.',
+                'attr' => [
+                    'class' => 'form-control input-width',
+                    'placeholder' => 'Enter email',
+                    'aria-describedby' => 'emailHelp'
+                ],
+                'help' => 'Nous ne partagerons jamais votre e-mail avec les autres.'
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit',
+                'attr' => ['class' => 'btn btn-primary button-top']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
