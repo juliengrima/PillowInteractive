83fce2f483a12389916d1a4fd4f3ed1d429e2c9a
juliengrima <gogrinformatique@gmail.com>
2024-07-08 16:05:11 +0200

    Remake NewsLetters controller

--- src/Entity/Games.php
+++ src/Entity/Games.php
@@ -21,22 +21,9 @@ class Games
 
     private ?string $description = null;
 
-    /**
-     * @var Collection<int, Platforms>
-     */
-    #[ORM\OneToMany(targetEntity: Platforms::class, mappedBy: 'gamePlateform')]
-    private Collection $plateforms;
-
-    /**
-     * @var Collection<int, Images>
-     */
-    #[ORM\OneToMany(targetEntity: Images::class, mappedBy: 'gamesImages')]
-    private Collection $images;
-
     public function __construct()
     {
-        $this->plateforms = new ArrayCollection();
-        $this->images = new ArrayCollection();
+
     }
 
     public function getId(): ?int
@@ -64,66 +51,6 @@ class Games
     public function setDescription(?string $description): static
     {
         $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Platforms>
-     */
-    public function getPlateforms(): Collection
-    {
-        return $this->plateforms;
-    }
-
-    public function addPlateform(Platforms $plateform): static
-    {
-        if (!$this->plateforms->contains($plateform)) {
-            $this->plateforms->add($plateform);
-            $plateform->setGamePlateform($this);
-        }
-
-        return $this;
-    }
-
-    public function removePlateform(Platforms $plateform): static
-    {
-        if ($this->plateforms->removeElement($plateform)) {
-            // set the owning side to null (unless already changed)
-            if ($plateform->getGamePlateform() === $this) {
-                $plateform->setGamePlateform(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Images>
-     */
-    public function getImages(): Collection
-    {
-        return $this->images;
-    }
-
-    public function addImage(Images $image): static
-    {
-        if (!$this->images->contains($image)) {
-            $this->images->add($image);
-            $image->setGamesImages($this);
-        }
-
-        return $this;
-    }
-
-    public function removeImage(Images $image): static
-    {
-        if ($this->images->removeElement($image)) {
-            // set the owning side to null (unless already changed)
-            if ($image->getGamesImages() === $this) {
-                $image->setGamesImages(null);
-            }
-        }
 
         return $this;
     }
