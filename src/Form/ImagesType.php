9707879713ba3da33801f9d3d872d7c5ee7c46fa
juliengrima <gogrinformatique@gmail.com>
2024-07-09 22:37:39 +0200

    design twig pages platforms

--- src/Form/ImagesType.php
+++ src/Form/ImagesType.php
@@ -7,6 +7,7 @@ use App\Entity\Images;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -29,6 +30,9 @@ class ImagesType extends AbstractType
             ->add('game', EntityType::class, [
                 'class' => Games::class,
                 'choice_label' => 'name',
+            ])
+            ->add('text', TextareaType::class, [
+                'label' => 'Text',
             ])
         ;
     }
