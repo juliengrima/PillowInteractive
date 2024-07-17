a9fd5fd17467edbbf1140ff6d552cb25b40b7417
juliengrima <gogrinformatique@gmail.com>
2024-07-17 11:09:31 +0200

    fix home base envlocal

--- .env.local.php
+++ .env.local.php
@@ -7,5 +7,6 @@ return array (
   'SYMFONY_DOTENV_PATH' => './.env',
   'APP_SECRET' => 'aeaf505228db7d0b9348f00df9c0432a',
   'DATABASE_URL' => 'mysql://grju2696_decide1983:!@D0nie21!@127.0.0.1:3306/grju2696_pillowinteractive?serverVersion=8.0.32&charset=utf8mb4',
+  # 'DATABASE_URL' => 'mysql://root:root@127.0.0.1:3306/pillow?serverVersion=8.0.32&charset=utf8mb4',
   'MESSENGER_TRANSPORT_DSN' => 'doctrine://default?auto_setup=0',
 );
